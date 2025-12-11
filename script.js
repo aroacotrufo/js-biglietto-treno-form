6b5349f33bff99b7c3763a7ef3c4ddcdf6fe2f1d
Aroa Cotrufo <aroa.c97@gmail.com>
2025-12-11 19:10:33 +0100

    feat

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+
+// Il programma dovrà chiedere all'utente il numero dei km che vuole percorrere e l'età del passeggero.
+
+// DATI:
+// Il prezzo del biglietto è definito in base ai km (0.21€ al km).
+// Va applicato uno sconto del 20% per i minorenni.
+// Va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali). 
+
+
+
 // selezioniamo tutti i campi (select/input)
 const nameField = document.getElementById('name-field');
 const kmField = document.getElementById('km-field');

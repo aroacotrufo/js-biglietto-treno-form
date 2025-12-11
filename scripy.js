ad83bbfbca620b5a7db1c33f71c8ed084cf8fe7f
Aroa Cotrufo <aroa.c97@gmail.com>
2025-12-11 17:45:43 +0100

    Inizio

---
+++ scripy.js
@@ -0,0 +1,9 @@
+// selezioniamo tutti i campi (select/input)
+const nameField = document.getElementById('name-field');
+const kmField = document.getElementById('km-field');
+
+// seleziono il form di riferimento
+const form = document.querySelector('form');
+
+
+
